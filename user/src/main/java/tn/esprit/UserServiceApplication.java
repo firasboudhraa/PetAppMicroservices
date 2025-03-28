a068b8ccefb44c0f066038402ac9be9bc55186f2
yasmine <yasminemzoughi4@gmail.com>
2025-03-28 02:27:26 +0100

    added the feign client

--- user/src/main/java/tn/esprit/UserServiceApplication.java
+++ user/src/main/java/tn/esprit/UserServiceApplication.java
@@ -2,6 +2,10 @@ package tn.esprit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@EnableFeignClients
 
 @SpringBootApplication
 public class UserServiceApplication {
