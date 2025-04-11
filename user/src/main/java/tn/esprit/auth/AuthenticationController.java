0aa0ddc2b10c30d4ce5042a9746a7f757b439c82
yasmine <yasminemzoughi4@gmail.com>
2025-04-11 02:02:41 +0100

    test api_gateway

--- user/src/main/java/tn/esprit/auth/AuthenticationController.java
+++ user/src/main/java/tn/esprit/auth/AuthenticationController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/user/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
