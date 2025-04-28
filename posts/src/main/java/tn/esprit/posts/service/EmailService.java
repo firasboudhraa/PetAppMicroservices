ea3c0604f8d3845d717b299d3284d7df6a341ccd
Aymen-thabet <Thabet.aymen1@gmail.com>
2025-04-28 19:59:11 +0100

    corrected mailing 2

--- posts/src/main/java/tn/esprit/posts/service/EmailService.java
+++ posts/src/main/java/tn/esprit/posts/service/EmailService.java
@@ -13,21 +13,6 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
-    public void sendPostDeletionEmail(String title, String author, String email) {
-        if (email != null && !email.isEmpty()) {
-            String subject = "Post Deleted Notification";
-            String content = "Hello " + author + ",\n\n" +
-                    "We wanted to let you know that your post titled \"" + title + "\" has been deleted by our admin team because it did not comply with our community guidelines.\n\n" +
-                    "If you believe this was a mistake, please contact us.\n\n" +
-                    "Thank you for your understanding.\n\n" +
-                    "FureverBuddy Admin Team";
-
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo(email);
-            message.setSubject(subject);
-            message.setText(content);
-
-<<<<<<< HEAD
     public void sendPostDeletionEmail(String title, String firstName, String email) {
         String subject = "Post Deleted Notification";
         String body = "Hello " + firstName + ",\n\n" +
@@ -42,9 +27,5 @@ public class EmailService {
         message.setText(body);
 
         mailSender.send(message);
-=======
-            mailSender.send(message);
-        }
->>>>>>> c3d9507ad79d261ff9347f2c2b56b58830a98c30
     }
 }
