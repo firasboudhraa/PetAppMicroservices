f2fc5d36cfdbfe86855557c0432775aea6ca64af
Ghada <ghadabcl490@gmail.com>
2025-05-10 11:00:03 +0100

    docker+vars

--- Marketplace/src/main/java/tn/esprit/marketplace/MarketplaceApplication.java
+++ Marketplace/src/main/java/tn/esprit/marketplace/MarketplaceApplication.java
@@ -1,5 +1,4 @@
 package tn.esprit.marketplace;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -11,5 +10,4 @@ public class MarketplaceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MarketplaceApplication.class, args);
     }
-
 }
