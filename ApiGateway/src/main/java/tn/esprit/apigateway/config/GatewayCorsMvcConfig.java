04942d77bb68ca5d0be35357f42d9f1e7eeba0ce
firasboudhraa <fboudhraa2@gmail.com>
2025-05-10 14:40:05 +0100

    fixing user

--- ApiGateway/src/main/java/tn/esprit/apigateway/config/GatewayCorsMvcConfig.java
+++ ApiGateway/src/main/java/tn/esprit/apigateway/config/GatewayCorsMvcConfig.java
@@ -14,7 +14,7 @@ public class GatewayCorsMvcConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("${FRONTEND-URL:http://localhost}:4200")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
