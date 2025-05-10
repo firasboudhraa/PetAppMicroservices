1a74bc913a213f1f176ceb8712098491efdb41ac
houssem22Chakroun <houssem.Chakroun@esprit.tn>
2025-05-10 10:46:13 +0100

    houssem

--- MedicalNotebook/src/main/java/tn/esprit/medicalnotebook/control/CarnetController.java
+++ MedicalNotebook/src/main/java/tn/esprit/medicalnotebook/control/CarnetController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/carnet")
-//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CarnetController {
     @Autowired
     ICarnetService carnetService;
